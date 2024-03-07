1054f5d3639fc2af40f6c23b50d7590a01c7e2f1
yuhsienchiang <yuhsien.sam.chiang@gmail.com>
2024-03-07 12:51:21 +1100

    Change keymaps and format files

--- nvim/lua/yuhsienchiang/core/keymaps.lua
+++ nvim/lua/yuhsienchiang/core/keymaps.lua
@@ -26,29 +26,15 @@ vim.keymap.set("o", "n", "'Nn'[v:searchforward]",      { expr = true, desc = "Ne
 vim.keymap.set("n", "N", "'nN'[v:searchforward].'zv'", { expr = true, desc = "Prev search result" })
 vim.keymap.set("x", "N", "'nN'[v:searchforward]",      { expr = true, desc = "Prev search result" })
 vim.keymap.set("o", "N", "'nN'[v:searchforward]",      { expr = true, desc = "Prev search result" })
--- cursor movement
-vim.keymap.set("n", "<C-Right>", "$",               { desc = "Far Right",  silent = true, noremap = true })
-vim.keymap.set("i", "<C-Right>", "<C-o>$",          { desc = "Far Right",  silent = true, noremap = true })
-vim.keymap.set("n", "<C-Left>",  smart_cursor_home, { desc = "Far Left",   silent = true, noremap = true })
-vim.keymap.set("i", "<C-Left>",  smart_cursor_home, { desc = "Far Left",   silent = true, noremap = true })
-vim.keymap.set("n", "<C-Up>",    "gg",              { desc = "Far Top",    silent = true, noremap = true })
-vim.keymap.set("i", "<C-Up>",    "<C-o>gg",         { desc = "Far Top",    silent = true, noremap = true })
-vim.keymap.set("n", "<C-Down>",  "G",               { desc = "Far Bottom", silent = true, noremap = true })
-vim.keymap.set("i", "<C-Down>",  "<C-o>G",          { desc = "Far Bottom", silent = true, noremap = true })
 
--- window manipulation
-vim.keymap.set("n", "<leader><Down>",   ":wincmd j<CR>",               { desc = "Move Down",              silent = true, noremap = true })
-vim.keymap.set("n", "<leader><Up>",     ":wincmd k<CR>",               { desc = "Move Up",                silent = true, noremap = true })
-vim.keymap.set("n", "<leader><Right>",  ":wincmd l<CR>",               { desc = "Move Right",             silent = true, noremap = true })
-vim.keymap.set("n", "<leader><Left>",   ":wincmd h<CR>",               { desc = "Move Left",              silent = true, noremap = true })
-vim.keymap.set("n", "<leader>wh",       ":wincmd h<CR>",               { desc = "Move Left",              silent = true, noremap = true })
-vim.keymap.set("n", "<leader>wj",       ":wincmd j<CR>",               { desc = "Move Down",              silent = true, noremap = true })
-vim.keymap.set("n", "<leader>wk",       ":wincmd k<CR>",               { desc = "Move Up",                silent = true, noremap = true })
-vim.keymap.set("n", "<leader>wl",       ":wincmd l<CR>",               { desc = "Move Right",             silent = true, noremap = true })
-vim.keymap.set("n", "<leader>w<Up>",    "<cmd>resize +4<cr>",          { desc = "Increase window height", silent = true, noremap = true })
-vim.keymap.set("n", "<leader>w<Down>",  "<cmd>resize -4<cr>",          { desc = "Decrease window height", silent = true, noremap = true })
-vim.keymap.set("n", "<leader>w<Left>",  "<cmd>vertical resize -4<cr>", { desc = "Decrease window width",  silent = true, noremap = true })
-vim.keymap.set("n", "<leader>w<Right>", "<cmd>vertical resize +4<cr>", { desc = "Increase window width",  silent = true, noremap = true })
+-- cursor movement
+vim.keymap.set("n",          "<C-Right>", "$",               { desc = "Far Right",  silent = true, noremap = true })
+vim.keymap.set("i",          "<C-Right>", "<C-o>$",          { desc = "Far Right",  silent = true, noremap = true })
+vim.keymap.set({ "n", "i" }, "<C-Left>",  smart_cursor_home, { desc = "Far Left",   silent = true, noremap = true })
+vim.keymap.set("n",          "<C-Up>",    "gg",              { desc = "Far Top",    silent = true, noremap = true })
+vim.keymap.set("i",          "<C-Up>",    "<C-o>gg",         { desc = "Far Top",    silent = true, noremap = true })
+vim.keymap.set("n",          "<C-Down>",  "G",               { desc = "Far Bottom", silent = true, noremap = true })
+vim.keymap.set("i",          "<C-Down>",  "<C-o>G",          { desc = "Far Bottom", silent = true, noremap = true })
 
 -- better up/down
 vim.keymap.set({ "n", "x" }, "j",      "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
@@ -59,6 +45,32 @@ vim.keymap.set({ "n", "x" }, "<Up>",   "v:count == 0 ? 'gk' : 'k'", { expr = tru
 -- Better indenting
 vim.keymap.set("v", "<", "<gv")
 vim.keymap.set("v", ">", ">gv")
+
+-- Window
+-- Window Navigation
+vim.keymap.set("n", "<leader>wh",       ":wincmd h<CR>",               { desc = "Move Left",              silent = true, noremap = true })
+vim.keymap.set("n", "<leader>wj",       ":wincmd j<CR>",               { desc = "Move Down",              silent = true, noremap = true })
+vim.keymap.set("n", "<leader>wk",       ":wincmd k<CR>",               { desc = "Move Up",                silent = true, noremap = true })
+vim.keymap.set("n", "<leader>wl",       ":wincmd l<CR>",               { desc = "Move Right",             silent = true, noremap = true })
+-- Window Resize
+vim.keymap.set("n", "<leader>w<Up>",    "<cmd>resize +4<cr>",          { desc = "Increase window height", silent = true, noremap = true })
+vim.keymap.set("n", "<leader>w<Down>",  "<cmd>resize -4<cr>",          { desc = "Decrease window height", silent = true, noremap = true })
+vim.keymap.set("n", "<leader>w<Left>",  "<cmd>vertical resize -4<cr>", { desc = "Decrease window width",  silent = true, noremap = true })
+vim.keymap.set("n", "<leader>w<Right>", "<cmd>vertical resize +4<cr>", { desc = "Increase window width",  silent = true, noremap = true })
+
+-- Buffer Navigation
+vim.keymap.set("n", "<leader>b<Left>",  ":bprevious<CR>",   { desc = "Prev buffer", silent = true, noremap = true })
+vim.keymap.set("n", "<leader>bh",       ":bprevious<CR>",   { desc = "Prev buffer", silent = true, noremap = true })
+vim.keymap.set("n", "<leader>b<Right>", ":bnext<CR>",       { desc = "Next buffer", silent = true, noremap = true })
+vim.keymap.set("n", "<leader>bl",       ":bnext<CR>",       { desc = "Next buffer", silent = true, noremap = true })
+vim.keymap.set("n", "<leader>bq",       ":bp<bar>bd #<CR>", { desc = "Quit buffer", silent = true, noremap = true })
+
+-- Tab Navigation
+vim.keymap.set("n", "<leader>t<Left>",  ":tabprevious<CR>", { desc = "Prev tab", silent = true, noremap = true })
+vim.keymap.set("n", "<leader>th",       ":tabprevious<CR>", { desc = "Prev tab", silent = true, noremap = true })
+vim.keymap.set("n", "<leader>t<Right>", ":tabnext<CR>",     { desc = "Next tab", silent = true, noremap = true })
+vim.keymap.set("n", "<leader>tl",       ":tabnext<CR>",     { desc = "Next tab", silent = true, noremap = true })
+vim.keymap.set("n", "<leader>tq",       ":tabclose<CR>",    { desc = "Quit tab", silent = true, noremap = true })
 
 -- Telescope
 vim.keymap.set("n", "<leader>ff", ":Telescope find_files<CR>",  { desc = "Find files",          silent = true, noremap = true })
@@ -99,23 +111,14 @@ vim.keymap.set("n",          "<leader>ghR", ":Gitsigns reset_buffer<CR>",      {
 vim.keymap.set("n",          "<leader>ghu", ":Gitsigns undo_stage_buffer<CR>", { desc = "Undo Stage Hunk", silent = true, noremap = true })
 vim.keymap.set("n",          "<leader>ghd", ":Gitsigns diffthis ~<CR>",        { desc = "Diff This",       silent = true, noremap = true })
 
--- Buffer Navigation
-vim.keymap.set("n", "<leader>b<Left>",  ":bprevious<CR>",   { desc = "Prev buffer", silent = true, noremap = true })
-vim.keymap.set("n", "<leader>b<Right>", ":bnext<CR>",       { desc = "Next buffer", silent = true, noremap = true })
-vim.keymap.set("n", "<leader>bq",       ":bp<bar>bd #<CR>", { desc = "Quit buffer", silent = true, noremap = true })
-
--- Tab Navigation
-vim.keymap.set("n", "<leader>t<Left>",  ":tabprevious<CR>", { desc = "Prev tab", silent = true, noremap = true })
-vim.keymap.set("n", "<leader>t<Right>", ":tabnext<CR>",     { desc = "Next tab", silent = true, noremap = true })
-vim.keymap.set("n", "<leader>tq",       ":tabclose<CR>",    { desc = "Quit tab", silent = true, noremap = true })
+-- Noice
+vim.keymap.set("n", "<leader>nd", ":NoiceDismiss<CR>", { desc = "Close Noice Notification", silent = true, noremap = true })
 
 -- Add redo break points
 vim.keymap.set("i", ",", ",<C-g>u", { desc = "undo break points", silent = true, noremap = true })
 vim.keymap.set("i", ".", ".<C-g>u", { desc = "undo break points", silent = true, noremap = true })
 vim.keymap.set("i", ";", ";<C-g>u", { desc = "undo break points", silent = true, noremap = true })
 
--- Noice
-vim.keymap.set("n", "<leader>nd", ":NoiceDismiss<CR>", { desc = "Close Noice Notification", silent = true, noremap = true })
 
 -- Quit shortcuts
 vim.keymap.set("n", "<leader>qq", "<cmd>wqa<CR>", { desc = "Quit all"    , silent = true, noremap = true })
