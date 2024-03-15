952df094cdc6d2cbdf0acb1b774971c7ec7b5a6a
yuhsienchiang <yuhsien.sam.chiang@gmail.com>
2024-03-15 15:58:27 +1100

    Add comment plugin

---
+++ nvim/lua/yuhsienchiang/plugins/comment.lua
@@ -0,0 +1,23 @@
+return {
+	"numToStr/Comment.nvim",
+	event = { "BufReadPost", "BufNewFile" },
+	dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
+	config = function()
+		local comment = require("Comment")
+		local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
+
+		comment.setup({
+			toggler = {
+				line = "<C-m>",
+			},
+			opleader = {
+				line = "<C-m>",
+			},
+			mappings = {
+				basic = true,
+				extra = false,
+			},
+			pre_hood = ts_context_commentstring.create_pre_hook(),
+		})
+	end,
+}
