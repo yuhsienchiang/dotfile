caebc02638bc73455b40388fc335cd33e1405cff
yuhsienchiang <yuhsien.sam.chiang@gmail.com>
2024-03-16 15:27:23 +1100

    simplify shortcuts

--- nvim/lua/yuhsienchiang/plugins/dashboard.lua
+++ nvim/lua/yuhsienchiang/plugins/dashboard.lua
@@ -14,17 +14,16 @@ return {
         logo = string.rep("\n", 4) .. logo .. "\n\n"
 
         local buttons = {
-            { desc = "New file",           icon = "   ", key = "n", action = "ene | startinsert"},
-            { desc = "Recent file (CWD)",  icon = "   ", key = "r", action = "lua require('telescope.builtin').oldfiles({only_cwd=true})"},
-            { desc = "Recent file",        icon = "   ", key = "R", action = "lua require('telescope.builtin').oldfiles({only_cwd=false})"},
-            { desc = "Explorer",           icon = "󰙅   ", key = "e", action = "NvimTreeOpen"},
-            { desc = "Find file",          icon = "  ", key = "f", action = "Telescope find_files"},
-            { desc = "Find word",          icon = " 󰊄 ", key = "w", action = "Telescope live_grep"},
-            { desc = "Restore last",       icon = "󰦛  ", key = "u", action = "lua require('persistence').load({last = true})"},
-            { desc = "Restore here",       icon = "󰦛  ", key = "U", action = "lua require('persistence').load()"},
-            { desc = "Lazy",               icon = "󰒲   ", key = "l", action = "Lazy"},
-            { desc = "Setting",            icon = "   ", key = "s", action = "e $MYVIMRC | cd %:p:h | NvimTreeOpen"},
-            { desc = "Quit",               icon = "   ", key = "q", action = "qa"},
+            { desc = "New file",     icon = "   ", key = "n", action = "ene | startinsert"},
+            { desc = "Recent file",  icon = "   ", key = "r", action = "lua require('telescope.builtin').oldfiles({only_cwd=true})"},
+            { desc = "Explorer",     icon = "󰙅   ", key = "e", action = "NvimTreeOpen"},
+            { desc = "Find file",    icon = "  ", key = "f", action = "Telescope find_files"},
+            { desc = "Find word",    icon = " 󰊄 ", key = "w", action = "Telescope live_grep"},
+            { desc = "Restore last", icon = "󰦛  ", key = "u", action = "lua require('persistence').load({last = true})"},
+            { desc = "Restore here", icon = "󰦛  ", key = "U", action = "lua require('persistence').load()"},
+            { desc = "Lazy",         icon = "󰒲   ", key = "l", action = "Lazy"},
+            { desc = "Setting",      icon = "   ", key = "s", action = "e $MYVIMRC | cd %:p:h | NvimTreeOpen"},
+            { desc = "Quit",         icon = "   ", key = "q", action = "qa"},
         }
 
         for id, button in ipairs(buttons) do
