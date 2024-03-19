b98ebcfbd031a263917ce433ffbb2351da1de287
yuhsienchiang <yuhsien.sam.chiang@gmail.com>
2024-03-19 15:13:02 +1100

    Disable which-key for come keymaps

--- nvim/lua/yuhsienchiang/plugins/which-key.lua
+++ nvim/lua/yuhsienchiang/plugins/which-key.lua
@@ -14,14 +14,12 @@ return {
 				registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
 				-- the presets plugin, adds help for a bunch of default keybindings in Neovim
 				-- No actual key bindings are created
-				spelling = {
-					enabled = false, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
-				},
+				spelling = { enabled = false }, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
 				presets = {
 					operators = false, -- adds help for operators like d, y, ...
 					motions = false, -- adds help for motions
-					text_objects = true, -- help for text objects triggered after entering an operator
-					windows = true, -- default bindings on <c-w>
+					text_objects = false, -- help for text objects triggered after entering an operator
+					windows = false, -- default bindings on <c-w>
 					nav = false, -- misc bindings to work with windows
 					z = true, -- bindings for folds, spelling and others prefixed with z
 					g = true, -- bindings for prefixed with g
