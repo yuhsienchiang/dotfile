0a2433d61aa1e394900b1e701eae5e9842894694
yuhsienchiang <yuhsien.sam.chiang@gmail.com>
2024-04-01 16:05:53 +1100

    Simplify lspsaga setting

--- nvim/lua/yuhsienchiang/plugins/lsp/lspconfig.lua
+++ nvim/lua/yuhsienchiang/plugins/lsp/lspconfig.lua
@@ -70,16 +70,12 @@ return {
             vim.lsp.protocol.make_client_capabilities(),
             cmp_nvim_lsp.default_capabilities()
         )
-        local keymap_on_attach = function(_, bufnr)
-            -- set keybinds
-            vim.keymap.set("n",          "<leader>df", "<cmd>Lspsaga finder<CR>",                   { desc = "Show references/definition", noremap = true, silent = true, buffer = bufnr }) -- show definition, references
-            vim.keymap.set("n",          "<leader>dd", "<cmd>Lspsaga peek_definition<CR>",          { desc = "Show definition", noremap = true, silent = true, buffer = bufnr }) -- see definition and make edits in window
-            vim.keymap.set("n",          "<leader>da", "<cmd>Lspsaga code_action<CR>",              { desc = "Show code actions", noremap = true, silent = true, buffer = bufnr }) -- see available code actions
-            vim.keymap.set("n",          "<leader>dc", "<cmd>Lspsaga show_cursor_diagnostics<CR>",  { desc = "Show diagnostics for cursor", noremap = true, silent = true, buffer = bufnr }) -- show diagnostics for cursor
-            vim.keymap.set("n",          "<leader>dl", "<cmd>Lspsaga show_line_diagnostics<CR>",    { desc = "Show diagnostics for line", noremap = true, silent = true, buffer = bufnr }) -- show  diagnostics for line
-            vim.keymap.set("n",          "<leader>db", "<cmd>Lspsaga show_buf_diagnostics<CR>",     { desc = "Show diagnostics for buffer", noremap = true, silent = true, buffer = bufnr })
-            vim.keymap.set("n",          "<leader>dh", "<cmd>Lspsaga hover_doc<CR>",                { desc = "Show hover doc under cursor", noremap = true, silent = true, buffer = bufnr }) -- show documentation for what is under cursor
-            vim.keymap.set({ "n", "i" }, "<C-s>",      "<cmd>lua vim.lsp.buf.signature_help()<CR>", { desc = "Show signature help", noremap = true, silent = true, buffer = bufnr })
+        local keymap_on_attach = function(_, _)
+            vim.keymap.set("n",          "<leader>df", "<cmd>Lspsaga finder<CR>",          { desc = "Show references/definition",  noremap = true, silent = true }) -- show definition, references
+            vim.keymap.set("n",          "<leader>dd", "<cmd>Lspsaga peek_definition<CR>", { desc = "Show definition",             noremap = true, silent = true }) -- see definition and make edits in window
+            vim.keymap.set("n",          "<leader>da", "<cmd>Lspsaga code_action<CR>",     { desc = "Show code actions",           noremap = true, silent = true }) -- see available code actions
+            vim.keymap.set("n",          "<leader>dh", "<cmd>Lspsaga hover_doc<CR>",       { desc = "Show hover doc under cursor", noremap = true, silent = true }) -- show documentation for what is under cursor
+            vim.keymap.set({ "n", "i" }, "<C-s>",      vim.lsp.buf.signature_help,         { desc = "Show signature help",         noremap = true, silent = true })
         end
 
         mason_lspconfig.setup_handlers({
--- nvim/lua/yuhsienchiang/plugins/lsp/lspsaga.lua
+++ nvim/lua/yuhsienchiang/plugins/lsp/lspsaga.lua
@@ -1,6 +1,7 @@
 return {
 	"nvimdev/lspsaga.nvim",
 	event = { "LspAttach" },
+    lazy = true,
 	dependencies = {
 		"nvim-treesitter/nvim-treesitter",
 		"nvim-tree/nvim-web-devicons",
@@ -8,7 +9,7 @@ return {
 	config = function()
 		require("lspsaga").setup({
 			ui = {
-				border = "rounded",
+				border = "single",
 				devicon = true,
 				winblend = 10,
 				expand = " ",
@@ -19,8 +20,8 @@ return {
 			},
 			-- keybinds for navigation in lspsaga window
 			scroll_preview = {
-				scroll_down = "<C-k>",
-				scroll_up = "<C-j>",
+				scroll_down = "<C-n>",
+				scroll_up = "<C-p>",
 			},
 			-- use enter to open file with definition preview
 			definition = {
@@ -28,6 +29,7 @@ return {
 					edit = "<C-i>", -- open the file
 					vsplit = "<C-v>",
 					split = "<C-s>",
+					tabnew = "<C-t>",
 					quit = "q", -- close when focusing the window
 					close = "<C-q>", -- close even if not focusing the window
 				},
@@ -37,7 +39,7 @@ return {
 				layout = "float",
 				silent = false,
 				keys = {
-					shuttle = "[w",
+					shuttle = "<TAB>",
 					toggle_or_open = "<CR>",
 					vsplit = "<C-v>",
 					split = "<C-s>",
@@ -46,48 +48,11 @@ return {
 					close = "<C-q>",
 				},
 			},
-			rename = {
-				in_select = true,
-				auto_save = false,
-				project_max_width = 0.5,
-				project_max_height = 0.5,
-				keys = {
-					quit = "<C-q>",
-					exec = "<CR>",
-					select = "x",
-				},
-			},
-			symbol_in_winbar = {
-				enable = false,
-				separator = " › ",
-				hide_keyword = true,
-				show_file = true,
-				folder_level = 2,
-				color_mode = false,
-				dely = 500,
-			},
-			outline = {
-				enable = false,
-			},
-			lightbulb = {
-				enable = false,
-			},
-			implement = {
-				enable = false,
-			},
-			beacon = {
-				enable = true,
-				frequency = 10,
-			},
+			symbol_in_winbar = { enable = false, },
+			outline = { enable = false },
+			lightbulb = { enable = false },
+			implement = { enable = false },
+			beacon = { enable = false },
 		})
-
-		vim.keymap.set("n",          "<leader>df", "<cmd>Lspsaga finder<CR>",                   { desc = "Show references/definition",  noremap = true, silent = true }) -- show definition, references
-		vim.keymap.set("n",          "<leader>dd", "<cmd>Lspsaga peek_definition<CR>",          { desc = "Show definition",             noremap = true, silent = true }) -- see definition and make edits in window
-		vim.keymap.set("n",          "<leader>da", "<cmd>Lspsaga code_action<CR>",              { desc = "Show code actions",           noremap = true, silent = true }) -- see available code actions
-		vim.keymap.set("n",          "<leader>dc", "<cmd>Lspsaga show_cursor_diagnostics<CR>",  { desc = "Show diagnostics for cursor", noremap = true, silent = true }) -- show diagnostics for cursor
-		vim.keymap.set("n",          "<leader>dl", "<cmd>Lspsaga show_line_diagnostics<CR>",    { desc = "Show diagnostics for line",   noremap = true, silent = true }) -- show  diagnostics for line
-		vim.keymap.set("n",          "<leader>db", "<cmd>Lspsaga show_buf_diagnostics<CR>",     { desc = "Show diagnostics for buffer", noremap = true, silent = true })
-		vim.keymap.set("n",          "<leader>dh", "<cmd>Lspsaga hover_doc<CR>",                { desc = "Show hover doc under cursor", noremap = true, silent = true }) -- show documentation for what is under cursor
-		vim.keymap.set({ "n", "i" }, "<C-s>",      "<cmd>lua vim.lsp.buf.signature_help()<CR>", { desc = "Show signature help",         noremap = true, silent = true })
 	end,
 }
