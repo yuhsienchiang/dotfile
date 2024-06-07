1ea7accc8aa9748cc97b89a58bc63c8a15935e77
yuhsienchiang <yuhsien.sam.chiang@gmail.com>
2024-06-07 20:28:46 +1000

    Update venv-selector to latest version

--- nvim/lua/yuhsienchiang/plugins/venv-selector.lua
+++ nvim/lua/yuhsienchiang/plugins/venv-selector.lua
@@ -1,15 +1,34 @@
 return {
-	"linux-cultist/venv-selector.nvim",
-	cmd = { "VenvSelect", "VenvSelectCached" },
-	dependencies = {
-		"neovim/nvim-lspconfig",
-		"nvim-telescope/telescope.nvim",
-	},
-	config = function()
-		local selector = require("venv-selector")
-
-		selector.setup({
-			pyenv_path = "/Users/yuhsienchiang/.pyenv/versions",
-		})
-	end,
+    "linux-cultist/venv-selector.nvim",
+    cmd = { "VenvSelect" },
+    branch = "regexp",
+    dependencies = {
+        "neovim/nvim-lspconfig",
+        "nvim-telescope/telescope.nvim",
+    },
+    config = function()
+        local selector = require("venv-selector")
+        selector.setup({
+            settings = {
+                options = {
+                    enable_cached_venvs = false,
+                    notify_user_on_venv_activation = true,
+                },
+                search = {
+                    pyenv = {
+                        command = "fd 'versions/([0-9a-z_.]+)/bin/python$' ~/.pyenv/versions -p -i -L --color never -d 3",
+                    },
+                    cwd = false,
+                    virtualenvs = false,
+                    hatch = false,
+                    poetry = false,
+                    anaconda_envs = false,
+                    anaconda_base = false,
+                    pipx = false,
+                    workspace = false,
+                    file = false,
+                },
+            },
+        })
+    end,
 }
