7a78d34ea0e32e4bff64e4fa8ffd8f174bb05e21
yuhsienchiang <yuhsien.sam.chiang@gmail.com>
2024-09-28 23:33:18 +1000

    Update zsh config

--- zsh/plugins/aliases.zsh
+++ zsh/plugins/aliases.zsh
@@ -19,6 +19,7 @@ alias fzfp="fzf-tmux -p 65%,60% --preview 'bat --color always --style='plain' {}
 alias vimi="fzfp --multi --print0 | xargs -0 -o nvim -O"
 alias brewi="brew formulae | fzf-tmux --multi -p 65%,60% --preview 'brew info {}' --preview-window=right,60% | xargs brew install"
 alias ubrewi="brew leaves | fzf-tmux --multi -p 65%,60% --preview 'brew info {}' --preview-window=right,60% | xargs brew uninstall"
+alias brewupdate="brew update && brew upgrade && brew cleanup"
 
 # replace ls with eza
 alias ls='eza --group-directories-first --color=always --icons=always --no-time --no-user'
--- zsh/plugins/exports.zsh
+++ zsh/plugins/exports.zsh
@@ -22,7 +22,7 @@ if [[ -z $TMUX ]]; then
     # FZF
     export FZF_DEFAULT_COMMAND='fd -tf -tl --hidden --follow --no-ignore-vcs'        # default: used by fzf commands without keymap
     export FZF_DEFAULT_OPTS=" \
-        --height 40% --layout=reverse --border=sharp --cycle \
+        --height 40% --layout=reverse --border=rounded --cycle \
         --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
         --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
         --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
