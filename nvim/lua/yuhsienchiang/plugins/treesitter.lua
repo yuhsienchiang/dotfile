6fbd0ab0860f66e361f8f948bcc78d5df1884922
yuhsienchiang <yuhsien.sam.chiang@gmail.com>
2024-10-23 17:59:20 +1100

    Merge two config files

--- nvim/lua/yuhsienchiang/plugins/treesitter.lua
+++ nvim/lua/yuhsienchiang/plugins/treesitter.lua
@@ -49,6 +49,32 @@ return {
             -- auto install above language parsers
             auto_install = true,
             vim.treesitter.language.register("bash", "zsh"),
+
+            -- text objects
+            textobjects = {
+                select = {
+                    enable = true,
+                    lookahead = true,
+                    keymaps = {
+                        ["a="] = { query = "@assignment.outer", desc = "Select outer part of an assignment" },
+                        ["i="] = { query = "@assignment.inner", desc = "Select inner part of an assignment" },
+                        ["L="] = { query = "@assignment.lhs",   desc = "Select left hand side of an assignment" },
+                        ["R="] = { query = "@assignment.rhs",   desc = "Select right hand side of an assignment" },
+                        ["aa"] = { query = "@parameter.outer",  desc = "Select outer part of a parameter/argument" },
+                        ["ia"] = { query = "@parameter.inner",  desc = "Select inner part of a parameter/argument" },
+                    },
+                    include_surrounding_whitespace = false,
+                },
+                swap = {
+                    enable = true,
+                    swap_next = {
+                        ["g>"] = { query = "@parameter.inner", desc = "Swap with prev argument" },
+                    },
+                    swap_previous = {
+                        ["g<"] = { query = "@parameter.inner", desc = "Swap with next argument" },
+                    },
+                },
+            },
         })
     end,
 }
