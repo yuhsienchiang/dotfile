a79fe2913b6a159b1e090209fd1951614cf1cc23
yuhsienchiang <yuhsien.sam.chiang@gmail.com>
2025-02-05 19:58:30 +1100

    Adding new plugin

---
+++ nvim/lua/yuhsienchiang/plugins/tiny-inline-diagnostic.lua
@@ -0,0 +1,28 @@
+return {
+    "rachartier/tiny-inline-diagnostic.nvim",
+    priority = 1000,
+    config = function()
+        require("tiny-inline-diagnostic").setup({
+            preset = "classic",
+            signs = {
+                left = "",
+                right = "",
+                diag = "● ",
+                arrow = "    ",
+                up_arrow = "    ",
+                vertical = " │",
+                vertical_end = " └",
+            },
+            blend = { factor = 0.15 },
+            options = {
+                show_source = true,
+                throttle = 5,
+                show_all_diags_on_cursorline = true,
+                multilines = {
+                    enabled = true,
+                    always_show = true,
+                },
+            },
+        })
+    end,
+}
