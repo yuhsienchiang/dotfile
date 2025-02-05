67e851fa12381e5fcb22afc1baa38b33f0b5c5cf
yuhsienchiang <yuhsien.sam.chiang@gmail.com>
2025-02-05 20:39:06 +1100

    Update breakchange config

--- nvim/lua/yuhsienchiang/plugins/markview.lua
+++ nvim/lua/yuhsienchiang/plugins/markview.lua
@@ -2,54 +2,61 @@ return {
     "OXY2DEV/markview.nvim",
     ft = { "markdown" },
     cmd = "Markview",
-    -- dependencies = {
-    --     "nvim-treesitter/nvim-treesitter",
-    --     "nvim-tree/nvim-web-devicons",
-    -- },
     opts = function()
         return {
-            highlight_groups = { "dynamic" },
-            hybrid_modes = { "i", "n" },
-            modes = { "n", "no", "c", "i" },
-            checkboxes = {
+            preview = {
                 enable = true,
-                checked = {
-                    text = "",
-                    hl = "MarkviewCheckboxChecked",
-                    scope_hl = "MarkviewCheckboxStriked",
-                },
-                unchecked = {
-                    text = "",
-                    hl = "MarkviewCheckboxUnchecked",
-                },
-                custom = {
-                    {
-                        match_string = "-",
-                        text = "",
-                        hl = "MarkviewCheckboxPending",
+                enable_hybrid_mode = true,
+                debounce = 150,
+                icon_provider = "devicons",
+                filetypes = { "markdown", "quarto", "rmd", "typst" },
+
+                -- modes to enable preview
+                --  { no }: pending
+                -- { "v", "vs" }: visual
+                -- { "V", "Vs" }: visual line
+                -- { "\22", "\22s" }: visual block
+                modes = { "n", "no", "c", "i", "v", "vs", "V", "Vs", "\22", "\22s" },
+
+                -- modes to enable hybrid preview.
+                -- modes in here must be present in "modes" to take effect
+                hybrid_modes = { "n", "no", "c", "i", "v", "vs", "V", "Vs", "\22", "\22s" },
+
+                draw_range = { vim.o.lines, vim.o.lines },
+                edit_range = { 0, 0 },
+                splitview_winopts = { split = "right" },
+            },
+            markdown_inline = {
+                enable = true,
+                checkboxes = {
+                    checked = {
+                        text = "",
+                        hl = "MarkviewCheckboxChecked",
+                        scope_hl = "MarkviewCheckboxStriked",
                     },
-                    {
-                        match_string = "~",
-                        text = "",
-                        hl = "MarkviewCheckboxProgress",
+                    unchecked = {
+                        text = "",
+                        hl = "MarkviewCheckboxUnchecked",
                     },
-                    {
-                        match_string = "o",
-                        text = "",
-                        hl = "MarkviewCheckboxCancelled",
+                    custom = {
+                        {
+                            match_string = "-",
+                            text = "",
+                            hl = "MarkviewCheckboxPending",
+                        },
+                        {
+                            match_string = "~",
+                            text = "",
+                            hl = "MarkviewCheckboxProgress",
+                        },
+                        {
+                            match_string = "o",
+                            text = "",
+                            hl = "MarkviewCheckboxCancelled",
+                        },
                     },
                 },
             },
         }
     end,
-    -- config = function()
-    --     local presets = require("markview.presets")
-    --     require("markview").setup({
-    --         highlight_groups = { "dynamic" },
-    --         hybrid_modes = { "i", "n" },
-    --         modes = { "n", "no", "c", "i" },
-    --
-    --         checkboxes = presets.checkboxes.nerd,
-    --     })
-    -- end,
 }
